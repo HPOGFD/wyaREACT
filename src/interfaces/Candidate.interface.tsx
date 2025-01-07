7077dc28d01e957dace89200f8e893a2262afb5d
Harry Oyarvide <poyarvide87@yahoo.com>
2025-01-07 14:57:14 -0500

    interface added

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,11 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface CandidateDetails {
+    avatar_url: string; // User's avatar image
+    login: string;      // GitHub username
+    name: string;       // Full name
+    location: string;   // User's location
+    email: string;      // Email address
+    company: string;    // Company user works at
+    bio: string;        // User bio
+  }
+  
